9944d99df7d8914e0f11ca437723efdb06b6c274
Farnakl <116156304+Farnakl@users.noreply.github.com>
2023-02-27 10:25:40 +0300

    Добавил калькулятор

---
+++ Function.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAk
+{
+    public class Function
+    {
+        public static double Plus(double second, double first)
+        {
+            double result = first + second;
+            return result;
+        }
+        public static double Minus(double second, double first)
+        {
+            double result = first - second;
+            return result;
+        }
+        public static double Multiplication(double second, double first)
+        {
+            double result = first * second;
+            return result;
+        }
+        public static double Division(double second, double first)
+        {
+            double result = first / second;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            double first = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите математическую операцию");
+            string operation = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double second = Convert.ToDouble(Console.ReadLine());
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = Function.Plus(second, first);
+                    break;
+                case "-":
+                    result = Function.Minus(second, first);
+                    break;
+                case "*":
+                    result = Function.Multiplication(second, first);
+                    break;
+                case "/":
+                    result = Function.Division(second, first);
+                    break;
+                default:
+                    Console.WriteLine("Responce: 404");
+                    break;
+            }
+            Console.WriteLine("Результат:" + " " + result);
+            Console.ReadKey();
+        }
+    }
+}
